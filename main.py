89e60cda534925a52a9e194d9d5508f857012cc3
rifigam <sd56tv@mail.ru>
2025-02-11 18:21:41 +0300

    fib

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def f1 (a):
-    return a%2==0
-print(f1(int(input())))
+def fib (a):
+    if a==1 or a==2: return 1
+    elif a==0: return 0
+    else:
+        return fib(a-1)+fib(a-2)
+print(fib(7))
